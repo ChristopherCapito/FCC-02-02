c4542f3bd44772ceb2c4a710faa84daa09c7636b
Ziegel999 <christopher@capito.rocks>
2019-12-05 00:55:03 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  FormControl
+} from 'react-bootstrap';
+import marked from 'marked';
+import htmlParser from 'html-react-parser';
 
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [htmlOutput, setHtmlOutput] = useState();
+
+  let somevar = React.createRef();
+
+  const handleChange = event => {
+    setTextInput(somevar.current.value);
+    let markdown = marked(textInput);
+    let htmlIntermediate = htmlParser(markdown);
+    setHtmlOutput(htmlIntermediate);
+  };
+
+  const add = (event) => console.log(event.keyCode)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container fluid='true'>
+        <Row style={{ minHeight: '100vh' }}>
+          <Col md={6} className='markdownEditor'>
+            <FormGroup controlId='markdownEditorField'>
+              <FormControl
+                as='textarea'
+                ref={somevar}            
+                onChange={() => handleChange()}
+                bsPrefix={'markdownEditorText'}
+              />
+            </FormGroup>
+          </Col>
+          <Col>{htmlOutput}</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
+
 
 export default App;
