aefdaebd2137fc362b102f0b7dc184048e09763e
Ziegel999 <christopher@capito.rocks>
2019-12-05 13:21:49 +0100

    Fixed State Issue. Added DOMpurify for sanitization

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {
-  Container,
-  Row,
-  Col,
-  Form,
-  FormGroup,
-  FormControl
-} from 'react-bootstrap';
+import { Container, Row, Col, FormGroup, FormControl } from 'react-bootstrap';
 import marked from 'marked';
 import htmlParser from 'html-react-parser';
+import DOMpurify from 'dompurify';
 
 function App() {
   const [textInput, setTextInput] = useState('');
   const [htmlOutput, setHtmlOutput] = useState();
 
-  let somevar = React.createRef();
+  useEffect(() => {
+    /** SET HTML OUTPUT FOR PREVIEW
+     * This function chain returns the html-parsed, sanitized, marked version of the input text.
+     * It works like this (see from inside out):
+     * 1. marked() takes the textInput with options
+     * 2. The result of marked gets sanitized with DOMpurify in order to prevent XSS
+     * The result of DOMpurify gets parsed via HTML parser
+     * to return valid html
+     */
+    setHtmlOutput(
+      htmlParser(
+        DOMpurify.sanitize(
+          marked(textInput, {
+            //MarkedJS Options
+            breaks: true
+          })
+        )
+      )
+    );
+  }, [textInput]);
 
-  const handleChange = event => {
-    setTextInput(somevar.current.value);
-    let markdown = marked(textInput);
-    let htmlIntermediate = htmlParser(markdown);
-    setHtmlOutput(htmlIntermediate);
-  };
-
-  const add = (event) => console.log(event.keyCode)
+  useEffect(() => {
+    console.log(htmlOutput);
+  }, [htmlOutput]);
 
   return (
     <div className='App'>
       <Container fluid='true'>
         <Row style={{ minHeight: '100vh' }}>
           <Col md={6} className='markdownEditor'>
-            <FormGroup controlId='markdownEditorField'>
+            <FormGroup>
               <FormControl
+                id='editor'
                 as='textarea'
-                ref={somevar}            
-                onChange={() => handleChange()}
+                onChange={event => setTextInput(event.target.value)}
                 bsPrefix={'markdownEditorText'}
               />
             </FormGroup>
           </Col>
-          <Col>{htmlOutput}</Col>
+          <Col id='preview' className='htmlOutputCol'>
+            {htmlOutput}
+          </Col>
         </Row>
       </Container>
     </div>
   );
 }
-
 
 export default App;
